70b13c536768bc69245283610efa7f536c6dc7f8
seanaleid <seanaleid@gmail.com>
2020-07-09 19:23:38 -0400

    Add item.py file

--- src/player.py
+++ src/player.py
@@ -2,9 +2,16 @@
 # currently.
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
 
     def __repr__(self):
         return f"name: {self.name}, current_room: {self.current_room}"
+
+    def get(self, item):
+        pass
+    
+    def drop(self, item):
+        pass
--- src/room.py
+++ src/room.py
@@ -2,16 +2,31 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, roomID, name, description, items):
+        self.roomID = roomID
         self.name = name
         self.description = description
-        self.n_to = None
-        self.e_to = None
-        self.s_to = None
-        self.w_to = None
+        self.items = []
+
+    def look(self):
+        foundItems = []
+
+        for item in self.items:
+            itemName = item[0]
+            print(f"You found {itemName}")
+            choice = input(f"Do you want to take it? y/n ")
+
+            if choice == 'y':
+                print(f"{itemName} added to your inventory")
+                self.items.pop(itemName)
+                foundItems = itemName
+            
+            elif choice == 'n':
+                print(f"You didn't take the item")
+
+            else:
+                print(f"Bad panda! =(")
+        return foundItems
 
     def __repr__(self):
         return f"name: {self.name}, description: {self.description}"
-
-
-print("test")
